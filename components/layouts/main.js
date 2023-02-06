4e0b33971e67ba6b6c74999c4c8089c6b7dca849
John Knowles <john.knowles.dev@gmail.com>
2023-02-06 15:09:37 -0500

    changed favicon

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -18,17 +18,11 @@ const Main = ({ children, router }) => {
         <meta name="description" content="John's homepage" />
         <meta name="author" content="John Knowles" />
         <meta name="author" content="Johnisadev" />
-        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="John Knowles" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
         <meta property="og:site_name" content="John Knowles" />
         <meta name="og:title" content="John Knowles" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:image" content="https://www.johnisadev/images/profile.jpg" />
         <title>Johnisadev - Homepage</title>
       </Head>
 
